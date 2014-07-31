7ec68ea0ce2f6c120b1a479a19ba3f83c150a368
Eric Zhang <ewicexclamationpoint@gmail.com>
2014-07-31 02:36:28 -0400

    created classes folder, started bracket class

---
+++ classes/bracket.php
@@ -0,0 +1,15 @@
+<?php
+
+class Bracket {
+	protected $organizers = array();
+
+	protected function addOrganizers( $user_id ) {
+
+	}
+
+	protected function getOrganizers() {
+		return $this->organizers;
+	}
+}
+
+?>
